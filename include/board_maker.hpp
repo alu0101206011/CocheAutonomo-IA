94319ea79151810ab8ab12d654596f2f6438c70f
Jorge Niebla Núñez <alu0101215457@ull.edu.es>
2020-10-30 19:24:52 +0000

    Add method CreateNewMap

--- include/board_maker.hpp
+++ include/board_maker.hpp
@@ -1,3 +1,7 @@
-
-
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
 void board_maker();
+void CreateNewMap(std::string&);
+void ModifyMap(std::string&);
--- src/board_maker.cpp
+++ src/board_maker.cpp
@@ -1,18 +1,20 @@
 #include "../include/board_maker.hpp"
 //#include "../include/board.hpp"
 
-#include <iostream>
-#include <string>
-
 void board_maker(){
-    std::cout << "HOLA" << std::endl;
+    std::string file;
     int option;
     std::cin >> option;
+    std::string filename;
 
     while (option != 5) {
         switch (option) {
             case 1:
                 /* Crear Nuevo Mapa */
+                std::cout << "Introduzca el nombre del mapa: ";
+                std::cin >> filename;
+                CreateNewMap(filename);
+                ModifyMap(filename);
                 break;
             case 2:
                 /* Modificar Mapa */
@@ -32,10 +34,45 @@ void board_maker(){
     }
 }
 
-void CreateNewMap(std::string FileName){
+void CreateNewMap(std::string &FileName) {
+    std::ofstream mapfile;
+    mapfile.open(FileName);
+    int height, wide;
+    std::cout << "¿Qué altura debe tener el tablero? ";
+    std::cin >> height;
+    height+=2;
+    std::cout << "¿Qué anchura debe tener el tablero? ";
+    std::cin >> wide;
+    wide+=2;
+    mapfile << wide << " " << height << std::endl;
+    std::vector<std::vector<int>> map(wide, std::vector<int>(height,0));
 
+    for (int i = 0; i < wide; i++) {
+        map[i][0] = 1;
+        map[i][height-1] = 1;
+    }
+    for (int i = 0; i < height; i++) {
+        map[0][i] = 1;
+        map[wide-1][i] = 1;
+    }
+
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < wide; j++) {
+            mapfile << map[j][i];
+            if (j != wide - 1) {
+                mapfile << " ";
+            }
+        }
+        if (i != height - 1) {
+            mapfile << std::endl;
+        }
+    }
+    mapfile.close();
 }
 
+void ModifyMap(std::string &FileName) {
+    
+}
 
 
 /* 12 12
