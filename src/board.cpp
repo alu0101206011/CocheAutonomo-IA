452e4d8241dddab60a0392ad3020fb7a98095446
Jorge Niebla Núñez <alu0101215457@ull.edu.es>
2020-11-03 17:20:43 +0000

    Add more changes to constructor

--- src/board.cpp
+++ src/board.cpp
@@ -10,8 +10,16 @@ Board::Board() {
 Board::Board(int M,int N) {
     M_ = M;
     N_ = N;
-    MatrixInt map(M_+2, std::vector<int>(N_+2));
+    MatrixInt map(M_+2, std::vector<int>(N_+2,0));
     MatrixBoard_= map;
+    for (int i = 0; i < N_; i++) {
+        map[i][0] = 1;
+        map[i][M_+1] = 1;
+    }
+    for (int i = 0; i < M_; i++) {
+        map[0][i] = 1;
+        map[N_+1][i] = 1;
+    }
 }
 
 Board::Board(std::string filename) {}
